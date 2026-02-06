55fceaa30d3a1bb86089a2dce1f1c69d22a1c959
randyDo404 <rdo@gr.grisd.net>
2026-02-06 08:41:16 -0600

    Added SparkMax to ShooterSubsystem

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,7 +1,15 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import static frc.robot.Constants.ShooterConstants.*;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
-    
+    private final SparkMax temp;
+
+    public ShooterSubsystem() {
+    temp = new SparkMax(SHOOTER_MOTOR_ID, MotorType.kBrushless);
+    }
 }
